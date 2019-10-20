df291d050ca1ed9b9025a0d6aa33a8ffbc3fd8c7
Thanatossan <kotokikung@gmail.com>
2019-10-20 14:06:55 +0700

    add font page

---
+++ resources/js/components/navbar.vue
@@ -0,0 +1,8 @@
+<<template>
+    <div>
+        <!-- As a link -->
+       <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+        </nav>
+    </div>
+</template>
