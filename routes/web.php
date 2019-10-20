3c5a732b0295ccf7c2bb6c976419fc9795502afa
chawitW <tiwahcer@DESKTOP-P8IUP53.localdomain>
2019-10-20 16:41:40 +0700

    Change route when_customers_login -> /customer replaced /home

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/homee', 'HomeController@index')->name('home');
-Route::get('/home', 'CustomerController@index');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/customer', 'CustomerController@index');
 
-    Route::prefix('admin')->group(function(){
+Route::prefix('admin')->group(function(){
         Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
         Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
         Route::get('/', 'AdminController@index')->name('admin.dashboard');
