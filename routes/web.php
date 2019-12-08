60738d739a4f6c437769a1ea4680f81e2d8b597d
Thanatossan <kotokikung@gmail.com>
2019-12-08 17:37:54 +0700

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/','shopController@index') -> name('shop.index');
-Route::get('/{product}','shopController@show')->name('shop.show');
+Route::get('/product/{product}','shopController@show')->name('shop.show');
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
